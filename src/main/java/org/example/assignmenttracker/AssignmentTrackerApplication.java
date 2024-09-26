1158b5aaafa6342266365a10c14e254d4e7c24ba
DoubtfulChicken <129512832+DoubtfulChicken@users.noreply.github.com>
2024-09-26 11:53:20 +1000

    Update AssignmentTrackerApplication.java

--- src/main/java/org/example/assignmenttracker/AssignmentTrackerApplication.java
+++ src/main/java/org/example/assignmenttracker/AssignmentTrackerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AssignmentTrackerApplication extends SpringBootServletInitializer {
+public class AssignmentTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AssignmentTrackerApplication.class, args);
